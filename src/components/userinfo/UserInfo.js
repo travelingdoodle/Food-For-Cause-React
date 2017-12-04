500b71fcf33cdd102290c6cb75dcf14a15275f3e
Cody Burmeister <cody.burmeister1@gmail.com>
2017-12-03 20:22:57 -0600

    modal overlay fix, added modal to carousel

--- src/components/userinfo/UserInfo.js
+++ src/components/userinfo/UserInfo.js
@@ -3,6 +3,15 @@ import "./UserInfo.css";
 import Modal from 'react-modal';
 
 const customStyles = {
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(0, 0, 0, 0.75)',
+    zIndex            : 1000
+  },
   content : {
     top                   : '50%',
     left                  : '50%',
