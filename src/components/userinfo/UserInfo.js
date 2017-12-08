f5f8c09e00d2e7bc785ce576893a56a39735558e
Reggie Brown <reggiebrown@gmail.com>
2017-12-08 14:44:43 -0600

    Revert "React design into conflict"

--- src/components/userinfo/UserInfo.js
+++ src/components/userinfo/UserInfo.js
@@ -49,66 +49,61 @@ export default class Main extends React.Component {
     this.setState({ modalIsOpen: false });
   }
 
-
-  componentWillMount() {
-    Modal.setAppElement('body');
- }
- 
   render() {
     return (
       <div>
         <main role="main">
-        <div className="container bio">
-          <div className="row">
-            <div className="col-lg-4 offset-lg-4">
-              <img className="rounded-circle" src="https://www.catholic.edu/assets/images/default_profile.jpg" alt="Placeholder" height="150" />
-              <h2><span>Name/Organization</span></h2>
-              <br />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-lg-8 col-md-10 offset-lg-2 text-left">
-              <h3>Contact information</h3>
-              <hr />
-              <table className="table contact">
-                <tbody>
-                  <tr>
-                    <td>
-                      <h5>Phone Number</h5>
-                    </td>
-                    <td><span>(555) 555-5555</span>
-                      <p><a className="btn btn-outline-info" role="button">Edit</a></p>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <h5>Email Address</h5>
-                    </td>
-                    <td><span>none@none.com</span>
-                      <p><a className="btn btn-outline-info" role="button">Edit</a></p>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <h5>Address</h5>
-                    </td>
-                    <td><span>123 N Main St<br /></span>
-                      <span>Kansas City, MO 64111<br /></span>
-                      <p><a className="btn btn-outline-info" role="button">Edit</a></p>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-                <hr />
-            </div>
-          </div>
-
-          <div className="container reserved">
+          <div className="container bio">
             <div className="row">
-              <div className="col-lg-8 offset-lg-2 text-left">
-                <h3>Reserved items</h3>
-                <div className="table-responsive">
+              <div className="col-lg-4 offset-lg-4">
+                <img className="rounded-circle" src="https://www.catholic.edu/assets/images/default_profile.jpg" alt="Placeholder" height="150" />
+                <h2><span>Name/Organization</span></h2>
+                <br />
+              </div>
+            </div>
+
+            <table className="table contact">
+              <div className="row">
+                <div className="col-lg-8 offset-lg-2 text-left">
+                  <h3>Contact information</h3>
+                  <hr />
+                  <tbody>
+                    <tr>
+                      <td>
+                        <h5>Phone Number</h5>
+                      </td>
+                      <td><span>(555) 555-5555</span>
+                        <p><a className="btn btn-outline-info" href="#" role="button">Edit</a></p>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <h5>Email Address</h5>
+                      </td>
+                      <td><span>none@none.com</span>
+                        <p><a className="btn btn-outline-info" href="#" role="button">Edit</a></p>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <h5>Address</h5>
+                      </td>
+                      <td><span>123 N Main St<br /></span>
+                        <span>Kansas City, MO 64111<br /></span>
+                        <p><a className="btn btn-outline-info" href="#" role="button">Edit</a></p>
+                      </td>
+                    </tr>
+                  </tbody>
+                  <hr />
+                </div>
+              </div>
+            </table>
+
+            <div className="container reserved">
+              <div className="row">
+                <div className="col-lg-8 offset-lg-2 text-left">
+                  <h3>Reserved items</h3>
+                  <div className="table-responsive">
                     <div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
                       <table className="table table-striped">
                         <thead>
@@ -142,68 +137,68 @@ export default class Main extends React.Component {
                       </table>
                     </div>
                   </div>
+                </div>
               </div>
             </div>
           </div>
 
           <div className="container listed">
-              <div className="row">
-                <div className="col-lg-8 offset-lg-2 text-left">
-                  <h3>Listed items</h3>
-                  <div className="table-responsive">
-                      <div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
-                        <table className="table table-striped">
-                          <thead>
-                            <tr>
-                              <th>#</th>
-                              <th>Item</th>
-                              <th>Category</th>
-                              <th>Quantity (Lbs)</th>
-                              <th>Expiration (Days)</th>
-                              <th />
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td>1,001</td>
-                              <td>Lorem</td>
-                              <td>ipsum</td>
-                              <td>dolor</td>
-                              <td>sit</td>
-                              <td><button type="button" className="btn btn-danger">Remove</button></td>
-                            </tr>
-                            <tr>
-                              <td>1,002</td>
-                              <td>amet</td>
-                              <td>consectetur</td>
-                              <td>adipiscing</td>
-                              <td>elit</td>
-                              <td><button type="button" className="btn btn-danger">Remove</button></td>
-                            </tr>
-                          </tbody>
-                        </table>
-                        <hr className="divider" />
-                      </div>
-                    </div>
+            <div className="row">
+              <div className="col-lg-8 offset-lg-2 text-left">
+                <h3>Listed items</h3>
+                <div className="table-responsive">
+                  <div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
+                    <table className="table table-striped">
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>Item</th>
+                          <th>Category</th>
+                          <th>Quantity (Lbs)</th>
+                          <th>Expiration (Days)</th>
+                          <th />
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>1,001</td>
+                          <td>Lorem</td>
+                          <td>ipsum</td>
+                          <td>dolor</td>
+                          <td>sit</td>
+                          <td><button type="button" className="btn btn-danger">Remove</button></td>
+                        </tr>
+                        <tr>
+                          <td>1,002</td>
+                          <td>amet</td>
+                          <td>consectetur</td>
+                          <td>adipiscing</td>
+                          <td>elit</td>
+                          <td><button type="button" className="btn btn-danger">Remove</button></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <hr className="divider" />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          </main>
-            <Modal
-              isOpen={this.state.modalIsOpen}
-              onAfterOpen={this.afterOpenModal}
-              onRequestClose={this.closeModal}
-              style={customStyles}
-              contentLabel="Example Modal"
-            >
-              <h2 className="modal-heading" ref={subtitle => this.subtitle = subtitle}>Are You Sure?</h2>
-              <button className="btn btn-default close-modal" onClick={this.closeModal}>close</button>
-              <form>
-                <p className="modal-inner"></p>
-                <button className="btn btn-danger">Confirm</button>
-              </form>
-            </Modal>
+        </main>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <h2 className="modal-heading" ref={subtitle => this.subtitle = subtitle}>Are You Sure?</h2>
+          <button className="btn btn-default close-modal" onClick={this.closeModal}>close</button>
+          <form>
+            <p className="modal-inner" />
+            <button className="btn btn-danger">Confirm</button>
+          </form>
+        </Modal>
       </div>
     );
   }
