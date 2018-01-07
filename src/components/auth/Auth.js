f4460e0afcfeccaae197cd15992b1fa17e3db631
travelingdoodle <reggiebrown@gmail.com>
2018-01-06 18:52:38 -0600

    updated auth.js

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -22,54 +22,54 @@ class Login extends React.Component {
   render() {
     return (
       <div className="container login">
-      <form className="form-signin" action="/users/login" method="post">
-      <h2 className="form-signin-heading text-center">Log in</h2>
-      <label htmlFor="inputEmail" className="sr-only">
+        <form className="form-signin" action="/users/login" method="post">
+          <h2 className="form-signin-heading text-center">Log in</h2>
+          <label htmlFor="inputEmail" className="sr-only">
         Email address
-      </label>
-      <input
-        onChange={this.handleInputChange}
-        value={this.email}
-        name="email"
-        type="email"
-        id="inputEmail"
-        className="form-control"
-        placeholder="Email address"
-        required
-        autoFocus
-      />
-      <label htmlFor="inputPassword" className="sr-only">
+          </label>
+          <input
+            onChange={this.handleInputChange}
+            value={this.email}
+            name="email"
+            type="email"
+            id="inputEmail"
+            className="form-control"
+            placeholder="Email address"
+            required
+            autoFocus
+        />
+          <label htmlFor="inputPassword" className="sr-only">
         Password
-      </label>
-      <input
-        onChange={this.handleInputChange}
-        value={this.password}
-        name="password"
-        type="password"
-        id="inputPassword"
-        className="form-control"
-        placeholder="Password"
-        required
-      />
-      <div className="checkbox">
-        <label>
-          <input type="checkbox" value="remember-me" /> Remember me
-        </label>
-      </div>
-      <button
-        className="btn btn-lg btn-secondary btn-block"
-        type="submit"
-        onClick={this.handleFormSubmit}
-      >
+          </label>
+          <input
+            onChange={this.handleInputChange}
+            value={this.password}
+            name="password"
+            type="password"
+            id="inputPassword"
+            className="form-control"
+            placeholder="Password"
+            required
+        />
+          <div className="checkbox">
+            <label>
+              <input type="checkbox" value="remember-me" /> Remember me
+            </label>
+          </div>
+          <button
+            className="btn btn-lg btn-secondary btn-block"
+            type="submit"
+            onClick={this.handleFormSubmit}
+        >
         Log in
-      </button>
-    </form>
-      <div id="create" className="text-center">
-      <p>
+          </button>
+        </form>
+        <div id="create" className="text-center">
+          <p>
         First time? <Link to="/register">Create an account</Link>
-      </p>
-    </div>
-    </div>
+          </p>
+        </div>
+      </div>
     );
   }
 }
